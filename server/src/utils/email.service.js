48ad69838c3dc1211b143308d042e6367ac5b104
Saran <rush4925@gmail.com>
2026-08-07 20:45:05 +0530

    error correction

--- server/src/utils/email.service.js
+++ server/src/utils/email.service.js
@@ -1,10 +1,11 @@
-import nodemailer from "nodemailer";
-import env from "../config/env.config.js";
+import nodemailer from 'nodemailer';
+import env from '../config/env.config.js';
 
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: Number(env.SMTP_PORT),
   secure: false,
+  requireTLS: true,
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASS,
@@ -12,6 +13,7 @@ const transporter = nodemailer.createTransport({
   connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 60000,
+  family: 4,
 });
 
 export const sendEmail = async (to, subject, html) => {
@@ -23,13 +25,13 @@ export const sendEmail = async (to, subject, html) => {
       html,
     });
 
-    console.info("[email] Email sent", {
+    console.info('[email] Email sent', {
       messageId: result.messageId,
     });
 
     return result;
   } catch (error) {
-    console.error("[email] sendEmail failed", {
+    console.error('[email] sendEmail failed', {
       code: error.code,
       message: error.message,
     });
